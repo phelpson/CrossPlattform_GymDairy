27b3361cb18378b173c7e8ec4881b26e12e9b678
phelpson <philipmayer777@gmail.com>
2019-12-23 09:58:00 +0100

    added basic structure

---
+++ main.js
@@ -0,0 +1,8 @@
+//Document Ready Function Post Render
+$(document).ready(function() {
+
+    // New Workout Button Listener
+    $("#newWorkout").click(function() {
+        window.location = 'workout.html';
+    });
+});
